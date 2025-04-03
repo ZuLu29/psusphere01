9cf486048ab44986b5359b0b0b60eb4e63680615
ZuLu29 <markneilconde850@gmail.com>
2025-04-03 22:11:18 +0800

    di magana css 03 na me

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from studentorg.views import HomePageView
+from studentorg import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", views.HomePageView.as_view(), name="home"),
 ]
