807dba8c7fdabf8c8620a87755b09fa8a91446b6
ZuLu29 <markneilconde850@gmail.com>
2025-04-03 23:42:07 +0800

    may bug parin

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -9,3 +9,10 @@ class HomePageView(ListView):
     model = Organization
     context_object_name = "home"
     template_name = "home.html"
+
+
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = "object_list"
+    template_name = "org_list.html"
+    paginate_by = 5
